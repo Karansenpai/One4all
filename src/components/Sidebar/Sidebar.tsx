32cb73024b27617bd1b708d7941a2975f854797a
atharva0192 <atharva0192@gmail.com>
2024-03-30 20:10:28 +0530

    Styles in Account page and Sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import style from "./Sidebar.module.css"
+import style from "./Sidebar.module.css";
 import SideBarComp from "./SidebarComponent/SideBarComp";
 const Sidebar = () => {
-  return <div className= {style.container}>
-
-    <SideBarComp/>
-    
-  </div>;
+  return (
+    <div className={`${style.container} dark:bg-[#121E31] bg-[#FFFFFF] `}>
+      <SideBarComp />
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/components/account/AccountForm.tsx
+++ src/components/account/AccountForm.tsx
@@ -1,11 +1,19 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { auth } from "@/auth";
-import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
-import { CardContent, Card, CardTitle, CardDescription, CardHeader } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Checkbox } from "@/components/ui/checkbox"
-import { Button } from "@/components/ui/button"
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+import {
+  CardContent,
+  Card,
+  CardTitle,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import styles from "./acc.module.css";
+import Image from "next/image";
 // import { Switch } from "@/components/ui/switch"
 
 export default function AccountForm({ session }: any) {
@@ -18,16 +26,16 @@ export default function AccountForm({ session }: any) {
             <CardDescription>Update your personal information.</CardDescription>
           </CardHeader>
           <CardContent>
-            <div className="grid grid-cols-3 items-start gap-4">
-              <div className="flex items-center space-x-4">
+            <div className={styles.container}>
+              <div className={`flex items-center space-x-4 ${styles.AvtText}`}>
                 <Label htmlFor="avatar">Avatar</Label>
               </div>
-              <div className="col-span-2">
-                <img
+              <div className={`col-span-2 ${styles.imgAvt}`}>
+                <Image
                   alt="Avatar"
                   className="rounded-full"
                   height="96"
-                  src="/placeholder.svg"
+                  src="/noavatar.png"
                   style={{
                     aspectRatio: "96/96",
                     objectFit: "cover",
@@ -60,52 +68,76 @@ export default function AccountForm({ session }: any) {
         <Card>
           <CardHeader>
             <CardTitle>Notification Preferences</CardTitle>
-            <CardDescription>Choose how you'd like to be notified.</CardDescription>
+            <CardDescription>
+              Choose how you'd like to be notified.
+            </CardDescription>
           </CardHeader>
           <CardContent>
             <fieldset>
-              <legend className="text-base font-medium">Email notifications</legend>
-              <div className="space-y-4">
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="all">
+              <legend className="text-base font-medium">
+                Email notifications
+              </legend>
+              <div className="my-5 space-y-4">
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="all"
+                  >
                     All notifications
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="mentions">
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="mentions"
+                  >
                     Only mentions
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="none">
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="none"
+                  >
                     Do not disturb
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
               </div>
             </fieldset>
             <fieldset className="mt-4">
-              <legend className="text-base font-medium">SMS notifications</legend>
-              <div className="space-y-4">
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="all-sms">
+              <legend className="text-base font-medium">
+                SMS notifications
+              </legend>
+              <div className="my-4 space-y-4">
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="all-sms"
+                  >
                     All notifications
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="mentions-sms">
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="mentions-sms"
+                  >
                     Only mentions
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
-                <div>
-                  <div />
-                  <Label className="ml-2 text-sm font-medium leading-6" htmlFor="none-sms">
-                    Do not disturb
+                <div className={styles.labels}>
+                  <Label
+                    className="ml-2 text-sm font-medium leading-6"
+                    htmlFor="none-sms"
+                  >
+                    Do not disturb{" "}
                   </Label>
+                  <Input className={`${styles.check}`} type="radio" />
                 </div>
               </div>
             </fieldset>
@@ -120,7 +152,11 @@ export default function AccountForm({ session }: any) {
             <div className="space-y-4">
               <div>
                 <Label htmlFor="password">Password</Label>
-                <Button className="ml-auto h-8 text-sm" id="password" variant="outline">
+                <Button
+                  className="ml-auto h-8 text-sm"
+                  id="password"
+                  variant="outline"
+                >
                   Change
                 </Button>
               </div>
@@ -132,5 +168,6 @@ export default function AccountForm({ session }: any) {
           </CardContent>
         </Card>
       </div>
-    </div>    );
+    </div>
+  );
 }
